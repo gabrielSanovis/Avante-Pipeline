e9175e08cde92427bb5601841bf81e9a78bafe41
gabrielSanovis <9novis9@gmail.com>
2022-11-25 21:13:35 -0300

    wip

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,21 @@
+import { useReducer } from 'react';
 import { Reset } from 'styled-reset'
-import { Logo, Container, WelcomeText } from './styles';
+import {
+    Logo,
+    Container,
+    WelcomeText,
+    FieldWrapper,
+    ChangeButton
+} from './styles';
+import { EnvelopeSimple, Eye, EyeSlash, ShieldStar } from "phosphor-react";
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../../style/theme';
 import { words } from './mock';
+import { Field } from '../../components/Field';
 export function SignIn() {
+    const [safety, setSafety] = useReducer((state: boolean) => {
+        return !state
+    }, true)
     return (
         <>
             <Reset />
@@ -11,6 +23,29 @@ export function SignIn() {
                 <Container>
                     <Logo />
                     <WelcomeText>{words.welcomeText.pt}</WelcomeText>
+
+                    <FieldWrapper>
+                        <Field.Root label="E-mail" errorMessage="Mensagem de erro">
+                            <Field.Icon>
+                                <EnvelopeSimple />
+                            </Field.Icon>
+                            <Field.Input placeholder='example@email.com' type="email" />
+                        </Field.Root>
+                    </FieldWrapper>
+
+                    <FieldWrapper>
+                        <Field.Root label="Senha" errorMessage="Mensagem de erro">
+                            <Field.Icon>
+                                <ShieldStar />
+                            </Field.Icon>
+                            <Field.Input placeholder='Digite aqui sua senha' type={safety ? "password" : "text"} />
+                            <ChangeButton onClick={() => setSafety()}>
+                                <Field.Icon>
+                                    {safety ? <EyeSlash /> : <Eye />}
+                                </Field.Icon>
+                            </ChangeButton>
+                        </Field.Root>
+                    </FieldWrapper>
                 </Container>
             </ThemeProvider>
         </>
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    padding-inline: 16px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: ${80 / 16}rem 1rem ${50 / 16}rem 1rem;
+    padding: ${80 / 16}rem 0rem ${50 / 16}rem 0rem;
+    width: 100vw;
+    text-align: center;
 `;
 
 export const Logo = styled.div`
@@ -14,12 +15,36 @@ export const Logo = styled.div`
     background-repeat: no-repeat;
     width: 100px;
     height: 100px;
-    margin-bottom: ${40/16}rem;
+    margin-bottom: ${40 / 16}rem;
 `;
 
 export const WelcomeText = styled.h3`
-    font-weight: 500;
+    font-weight: ${({ theme: { fonts } }) => fonts.weight.medium};
     font-size: 24px;
     line-height: 28px;
-    color: ${({theme}) => theme.white.colors.WHITE[900]};
+    color: ${({ theme: { white } }) => white.colors.WHITE[900]};
+    text-shadow: 0px 4px 9px rgba(0, 0, 0, 0.25);
+    margin-bottom: ${70/16}rem;
+`;
+
+export const FieldWrapper = styled.div`
+    width: 90%;
+    max-width: 500px;
+    margin-bottom: ${48/16}rem;
+`;
+
+export const ChangeButton = styled.button`
+    border: none;
+    padding: 0;
+    color: transparent;
+    border-radius: 25px;
+    background-color: transparent;
+    cursor: pointer;
+    height: 32px;
+    :focus {
+        outline: none;
+    }
+    :hover {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
 `;
