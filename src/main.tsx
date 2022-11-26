306e4142460611ec92a8ca06fe76b619855e930c
gabrielSanovis <9novis9@gmail.com>
2022-11-26 16:50:39 -0300

    Created screen sign up

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import ReactDOM from "react-dom/client";
 // import { App } from "./pages/App";
 import {SignIn} from './pages/SignIn'
+import { SignUp } from "./pages/SignUp";
 import {GlobalStyle} from './style/styles'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
-    <SignIn />
+    <SignUp />
   </React.StrictMode>
 );
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,93 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-
+import { useReducer } from 'react';
+import { Reset } from 'styled-reset'
+import {
+    Container,
+    RegistrationText,
+    Form,
+    FieldWrapper,
+    ChangeButton,
+    ButtonsWrapper,
+} from './styles';
+import { EnvelopeSimple, Eye, EyeSlash, ShieldStar, User } from "phosphor-react";
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../style/theme';
+import { words } from './mock';
+import { Field } from '../../components/Field';
+import { Button } from '../../components/Button';
 export function SignUp() {
-  return <Button variant="contained">SignUp</Button>;
+    const [safety, setSafety] = useReducer((state: boolean) => {
+        return !state
+    }, true)
+    const [safetyConfirm, setSafetyConfirm] = useReducer((state: boolean) => {
+        return !state
+    }, true)
+    return (
+        <>
+            <Reset />
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <RegistrationText>{words.registeTitle.pt}</RegistrationText>
+                    <Form>
+                        <FieldWrapper>
+                            <Field.Root label="Nome" errorMessage="Mensagem de erro">
+                                <Field.Icon>
+                                    <User />
+                                </Field.Icon>
+                                <Field.Input placeholder='Digite aqui seu nome' type="email" />
+                            </Field.Root>
+                        </FieldWrapper>
+                        <FieldWrapper>
+                            <Field.Root label="Sobrenome" errorMessage="Mensagem de erro">
+                                <Field.Icon>
+                                    <User />
+                                </Field.Icon>
+                                <Field.Input placeholder='Digite aqui seu sobrenome' type="email" />
+                            </Field.Root>
+                        </FieldWrapper>
+                        <FieldWrapper>
+                            <Field.Root label="E-mail" errorMessage="Mensagem de erro">
+                                <Field.Icon>
+                                    <EnvelopeSimple />
+                                </Field.Icon>
+                                <Field.Input placeholder='example@email.com' type="email" />
+                            </Field.Root>
+                        </FieldWrapper>
+
+                        <FieldWrapper>
+                            <Field.Root label="Senha" errorMessage="Mensagem de erro">
+                                <Field.Icon>
+                                    <ShieldStar />
+                                </Field.Icon>
+                                <Field.Input placeholder='Digite aqui sua senha' type={safety ? "password" : "text"} />
+                                <ChangeButton onClick={() => setSafety()} type="button">
+                                    <Field.Icon>
+                                        {safety ? <EyeSlash /> : <Eye />}
+                                    </Field.Icon>
+                                </ChangeButton>
+                            </Field.Root>
+                        </FieldWrapper>
+
+                        <FieldWrapper>
+                            <Field.Root label="Confirma senha" errorMessage="Mensagem de erro">
+                                <Field.Icon>
+                                    <ShieldStar />
+                                </Field.Icon>
+                                <Field.Input placeholder='Digite aqui sua senha' type={safety ? "password" : "text"} />
+                                <ChangeButton onClick={() => setSafetyConfirm()} type="button">
+                                    <Field.Icon>
+                                        {safetyConfirm ? <EyeSlash /> : <Eye />}
+                                    </Field.Icon>
+                                </ChangeButton>
+                            </Field.Root>
+                        </FieldWrapper>
+
+                        <ButtonsWrapper>
+                            <Button larger="sm" variant='fill' type='button'>Voltar</Button>
+                            <Button larger="sm" variant='outline' type='submit'>Confirmar</Button>
+                        </ButtonsWrapper>
+                    </Form>
+                </Container>
+            </ThemeProvider>
+        </>
+    );
 }
