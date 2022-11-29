3ab91d51f848075f234e96163ebe556b4a884b38
gabrielSanovis <9novis9@gmail.com>
2022-11-29 14:48:20 -0300

    basename

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -65,7 +65,7 @@ export function App() {
     <>
       <GlobalStyle isOpen={isOpen} />
 
-      <Router>
+      <Router basename="https://gabrielsanovis.github.io/Avante-Pipeline/" >
         <Routes>
           <Route path="/" element={<Home />} />
 
